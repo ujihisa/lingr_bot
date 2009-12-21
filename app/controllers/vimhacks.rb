cdfd64f48844a6462fa8aa890a2b2a1238a85df8
ujihisa <ujihisa@gmail.com>
2009-12-20 18:09:22 -0800

    Simplify

--- app/controllers/vimhacks.rb
+++ app/controllers/vimhacks.rb
@@ -5,12 +5,11 @@ end
 post '/vimhacks' do
   v = VimHacks.new
   JSON.parse(params[:json])["events"].select {|e| e['message'] }.inject([]) {|re, e|
-    case e['message']['text']
-    when /^:vimh(acks)?\s+(\S.*)$/
-      re += v.search($2).map(&:to_s)
-    when /^:vimh(acks)?$/
-      re += v.recent[0..8].map(&:to_s)
-    end
-    re
+    re + case e['message']['text']
+      when /^:vimh(acks)?\s+(\S.*)$/
+        v.search($2).map(&:to_s)
+      when /^:vimh(acks)?$/
+        v.recent[0..8].map(&:to_s)
+      end
   }.join "\n"
 end
