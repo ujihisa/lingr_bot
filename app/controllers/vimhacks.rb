02f1ba04d8873d05fea752d2fa4356ff28874593
ujihisa <ujihisa@gmail.com>
2009-12-20 18:00:52 -0800

    Uses guard

--- app/controllers/vimhacks.rb
+++ app/controllers/vimhacks.rb
@@ -7,17 +7,16 @@ post '/vimhacks' do
   json = JSON.parse(params[:json])
   v = VimHacks.new
   json["events"].each do |e|
-    if e['message']
-      # m = e['message']['text'].force_encoding(Encoding::ASCII_8BIT)
-      m = e['message']['text']
-      if /^:vimh(acks)?\s+(\S.*)$/ =~ m
-        v.search($1).map{ |x| x.to_s }.each do |h|
-          re += h + "\n"
-        end
-      elsif /^:vimh(acks)?$/ =~ m
-        v.recent[0..8].map{ |x| x.to_s }.each do |h|
-          re += h + "\n"
-        end
+    next unless e['message']
+    # m = e['message']['text'].force_encoding(Encoding::ASCII_8BIT)
+    m = e['message']['text']
+    if /^:vimh(acks)?\s+(\S.*)$/ =~ m
+      v.search($1).map{ |x| x.to_s }.each do |h|
+        re += h + "\n"
+      end
+    elsif /^:vimh(acks)?$/ =~ m
+      v.recent[0..8].map{ |x| x.to_s }.each do |h|
+        re += h + "\n"
       end
     end
   end
