492f7cc58bfc9f9c1228fbc16ad04bf5c4542114
ujihisa <ujihisa@gmail.com>
2009-12-20 18:18:16 -0800

    Golf

--- app/controllers/vimhacks.rb
+++ app/controllers/vimhacks.rb
@@ -1,15 +1,8 @@
-get '/vimhacks' do
-  "hi I'm a bot"
-end
-
-post '/vimhacks' do
-  v = VimHacks.new
-  JSON.parse(params[:json])["events"].select {|e| e['message'] }.map {|e|
-    case e['message']['text']
-    when /^:vimh(acks)?\s+(\S.*)$/
-      v.search($2)
-    when /^:vimh(acks)?$/
-      v.recent.take(9)
-    end.map(&:to_s)
-  }.flatten.compact.join("\n")
-end
+get(a='/vimhacks'){"hi I'm a bot"}
+post(a){v=VimHacks.new
+JSON.parse(params[:json])["events"].select{|e|e['message']}.map{|e|case e['message']['text']
+when /^:vimh(acks)?\s+(\S.*)$/
+v.search $2
+when /^:vimh(acks)?$/
+v.recent.take 9
+end.map &:to_s}.flatten.compact.join"\n"}
