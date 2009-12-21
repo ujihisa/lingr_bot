c74f400a7a4fe2735e6f9c2f25eb166965963f9a
ujihisa <ujihisa@gmail.com>
2009-12-20 18:10:26 -0800

    Map is enough

--- app/controllers/vimhacks.rb
+++ app/controllers/vimhacks.rb
@@ -4,12 +4,12 @@ end
 
 post '/vimhacks' do
   v = VimHacks.new
-  JSON.parse(params[:json])["events"].select {|e| e['message'] }.inject([]) {|re, e|
-    re + case e['message']['text']
-      when /^:vimh(acks)?\s+(\S.*)$/
-        v.search($2).map(&:to_s)
-      when /^:vimh(acks)?$/
-        v.recent[0..8].map(&:to_s)
-      end
-  }.join "\n"
+  JSON.parse(params[:json])["events"].select {|e| e['message'] }.map {|e|
+    case e['message']['text']
+    when /^:vimh(acks)?\s+(\S.*)$/
+      v.search($2).map(&:to_s)
+    when /^:vimh(acks)?$/
+      v.recent[0..8].map(&:to_s)
+    end
+  }.inject(:+).join "\n"
 end
