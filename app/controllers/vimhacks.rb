d134b7e4554940fd52101beb7024ee3e39951a98
ujihisa <ujihisa@gmail.com>
2009-12-20 18:06:51 -0800

    Uses select instead of guard

--- app/controllers/vimhacks.rb
+++ app/controllers/vimhacks.rb
@@ -5,10 +5,8 @@ end
 post '/vimhacks' do
   re = []
   v = VimHacks.new
-  JSON.parse(params[:json])["events"].each do |e|
-    next unless e['message']
-    m = e['message']['text']
-    case m
+  JSON.parse(params[:json])["events"].select {|e| e['message'] }.each do |e|
+    case e['message']['text']
     when /^:vimh(acks)?\s+(\S.*)$/
       v.search($2).map(&:to_s).each do |h|
         re << h
